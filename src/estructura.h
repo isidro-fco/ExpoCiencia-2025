ed16468417e53d5954967dafc9f6435a46efacb2
isidro_fco <isidrofranciscoperezpaz@gmail.com>
2025-10-03 09:46:40 -0700

    version 1.2

--- src/estructura.h
+++ src/estructura.h
@@ -9,3 +9,19 @@
 #define ALTO_PANTALLA 1080
 #define ANCHO_FONDO 3840
 #define ALTO_FONDO 2160
+
+typedef struct _cancion
+{
+    char titulo[65];
+    char artista[45];
+    char duracion[8];
+    char portada[255];
+    char audio[255];
+    char video[255];
+
+    struct _cancion *siguiente;
+    struct _cancion *anterior;
+} Cancion;
+
+typedef Cancion *NodoPtr;
+typedef Cancion *lista;
